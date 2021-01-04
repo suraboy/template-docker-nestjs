4426760573114090fceac7970be4e6bdedaac915
Sirichai Janpan <sirichai.jan@ascendcorp.com>
2021-01-05 02:44:04 +0700

    setup docker nest js

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World NestJs!';
+    return 'Hello World Nest Js by Suraboy!';
   }
 }
